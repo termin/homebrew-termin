c22417d11855991e930e449e80a0d438bf886661
termin <terminal.dot.dev+pub@gmail.com>
2014-10-18 07:07:12 +0900

    sha

--- erutaso.rb
+++ erutaso.rb
@@ -3,7 +3,7 @@ require "formula"
 class Erutaso < Formula
   homepage 'https://github.com/sgymtic/sl'
   url 'https://github.com/sgymtic/sl/archive/ed5b15fd3c68946cff8dee629474f7fe01d67c56.zip'
-  sha1 '1cc16b218fd171a99057b828b0a3e9b50b48b5df'
+  sha256 'ba434d9f17031f4d09f2fef53e1fab5fe5b169bcf650036a4ecea44ebcf64cac'
   head 'https://github.com/sgymtic/sl.git'
   version '5.0'
 

b4c525ab837eb3cdc21c281ec51bf9969cbd8c80
termin <terminal.dot.dev+pub@gmail.com>
2015-10-03 08:49:42 +0900

    erutaso shasum変わった？

--- erutaso.rb
+++ erutaso.rb
@@ -3,7 +3,7 @@ require "formula"
 class Erutaso < Formula
   homepage 'https://github.com/sgymtic/sl'
   url 'https://github.com/sgymtic/sl/archive/ed5b15fd3c68946cff8dee629474f7fe01d67c56.zip'
-  sha256 'ba434d9f17031f4d09f2fef53e1fab5fe5b169bcf650036a4ecea44ebcf64cac'
+  sha256 '57a927aee89804b7d270de85fa9baacded13e8f868ca944ee2c3a849758c5b69'
   head 'https://github.com/sgymtic/sl.git'
   version '5.0'
 
